2a4ce5c82ae48ea0d24752610da53be93b640249
Biprodas Roy <bipro10cse@gmail.com>
2018-05-03 13:50:45 +0600

    JAVA

--- Template/Template - JAVA/petr-Java8Templet.java
+++ Template/Template - JAVA/petr-Java8Templet.java
@@ -8,10 +8,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.InputStream;
 
-/**
+/***
  * Built using CHelper plug-in
  * Actual solution is at the top
- */
+ ***/
 public class Main {
     public static void main(String[] args) {
         InputStream inputStream = System.in;
